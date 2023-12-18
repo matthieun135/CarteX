f5a5effaf21ea3f32542776816f0f834a0b00cab
Nakavasi <119505794+Nakavasi@users.noreply.github.com>
2023-12-18 16:00:33 +0100

    Update inscription.php
    réglage des cookies

--- inscription.php
+++ inscription.php
@@ -3,28 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="inscription.css">
     <title>Inscription</title>
 </head>
 <body>
+    <a href="Accueil.html"><button class="bouton" id="btn-Accueil">Accueil</button></a>
     <h2>Bienvenue sur la page Inscription</h2>
     <?php
+    unset($_COOKIE["rang"]);
     if (isset($_POST["pseudo"])){
+        unset($_COOKIE["role"]);
+        $exist=FALSE;
         include("connexionBDD.php");
         $requete = $connexion->query("SELECT * FROM user");
         $utilisateur= $requete->fetchAll(PDO::FETCH_ASSOC);
         for ($i=0; $i < sizeof($utilisateur); $i++) { 
             if (password_verify($_POST["pseudo"], $utilisateur[$i]["identifiant"]) 
-            && password_verify($_POST["mot_de_passe"], $utilisateur[$i]["pwd"])){
+            || password_verify($_POST["mot_de_passe"], $utilisateur[$i]["pwd"])){
+                $exist=True;
                 break;
             }
         }
-        $requete= $connexion->prepare("INSERT INTO user (identifiant,pwd,rang)  VALUES (?,?,?)");
-        $identifiant=password_hash($_POST["pseudo"], PASSWORD_DEFAULT);
-        $pwd=password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
-        $rang="utilisateur";
-        $requete->execute([$identifiant,$pwd,$rang]);
-        setcookie("rang", "utilisateur");
-        header("Location: Carte.php");
+        if(!$exist){
+            $requete= $connexion->prepare("INSERT INTO user (identifiant,pwd,rang)  VALUES (?,?,?)");
+            $identifiant=password_hash($_POST["pseudo"], PASSWORD_DEFAULT);
+            $pwd=password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
+            $rang="utilisateur";
+            $requete->execute([$identifiant,$pwd,$rang]);
+            setcookie("rang", "utilisateur");
+            header("Location: Carte.php");
+        }
     }
     ?>
 <form method="post" action="inscription.php">

ab507bcd43857a910985b375b2afa3c0fe27e3aa
khaolakkkkk <khaola.najar@gmail.com>
2023-12-18 14:58:39 +0100

    finalisation pour afficher les carte pokemon et le filtrage

--- my-api/src/Affichage.js
+++ my-api/src/Affichage.js
@@ -1,35 +1,93 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const YuGiOhCardList = () => {
-  const [cardList, setCardList] = useState([]);
+const ListeCartesYuGiOh = () => {
+  const [listeCartes, setListeCartes] = useState([]);
+  const [termeRecherche, setTermeRecherche] = useState('');
+  const [typeFiltre, setTypeFiltre] = useState('nom'); // Par défaut, tri par nom
+  const [ordreTri, setOrdreTri] = useState('asc'); // Par défaut, tri croissant
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php');
-        setCardList(response.data.data);
-      } catch (error) {
-        console.error('Erreur lors de la récupération de la liste de cartes Yu-Gi-Oh! :', error);
+        const cartes = response.data.data.slice(0, 300); // Limiter à seulement les 300 premières cartes
+        setListeCartes(cartes);
+      } catch (erreur) {
+        console.error('Erreur lors de la récupération de la liste de cartes Yu-Gi-Oh! :', erreur);
       }
     };
 
     fetchData();
   }, []);
 
+  // Filtrer et trier les cartes en fonction des termes et des options de tri
+  const cartesFiltreesEtTrie = listeCartes
+    .filter((carte) => carte.name.toLowerCase().includes(termeRecherche.toLowerCase()))
+    .sort((a, b) => {
+      switch (typeFiltre) {
+        case 'nom':
+          return ordreTri === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+        case 'prix':
+          const prixA = a.card_prices ? parseFloat(a.card_prices[0].cardmarket_price) : 0;
+          const prixB = b.card_prices ? parseFloat(b.card_prices[0].cardmarket_price) : 0;
+          return ordreTri === 'asc' ? prixA - prixB : prixB - prixA;
+        case 'rarete':
+          const rareteA = a.card_prices ? a.card_prices[0].rarity : 'N/A';
+          const rareteB = b.card_prices ? b.card_prices[0].rarity : 'N/A';
+          return ordreTri === 'asc' ? rareteA.localeCompare(rareteB) : rareteB.localeCompare(rareteA);
+        default:
+          return 0;
+      }
+    });
 
   return (
     <div>
-      <h1>Yu-Gi-Oh! Card Liste</h1>
+      <h1>Liste de Cartes Yu-Gi-Oh!</h1>
+
+      {/* Barre de recherche */}
+      <input
+        type="text"
+        placeholder="Rechercher par nom"
+        value={termeRecherche}
+        onChange={(e) => setTermeRecherche(e.target.value)}
+      />
+
+      {/* Options de tri */}
+      <div>
+        <label>
+          Trier par :
+          <select value={typeFiltre} onChange={(e) => setTypeFiltre(e.target.value)}>
+            <option value="nom">Nom</option>
+            <option value="prix">Prix</option>
+            <option value="rarete">Rareté</option>
+          </select>
+        </label>
+        <label>
+          Ordre :
+          <select value={ordreTri} onChange={(e) => setOrdreTri(e.target.value)}>
+            <option value="asc">Croissant</option>
+            <option value="desc">Décroissant</option>
+          </select>
+        </label>
+      </div>
+
       <ul>
-        {cardList.map((card, index) => (
-          <li key={index}>
-            {card.name}
+        {cartesFiltreesEtTrie.map((carte) => (
+          <li key={carte.id}>
+            <img src={carte.card_images[0].image_url} alt={carte.name} />
+            <div>
+              <p>ID : {carte.id}</p>
+              <p>Nom : {carte.name}</p>
+              <p>Prix : {carte.card_prices ? carte.card_prices[0].cardmarket_price : 'N/A'}</p>
+            </div>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+  
 
-export default YuGiOhCardList;
+export default ListeCartesYuGiOh;
+
--- my-api/src/connexion.php
+++ my-api/src/connexion.php
@@ -1,7 +1,7 @@
 <?php
 $DB_hote = "localhost";
-$DB_utilisateur="";
-$DB_motDePasse="";
-$DB_nomDeLaBase="TPUE1";
+$DB_utilisateur="root";
+$DB_motDePasse="khaola";
+$DB_nomDeLaBase="CarteX";
 
 ?>

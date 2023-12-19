9dc2b8791aa5a23f8cc12860dde3b6280185410a
khaolakkkkk <121861034+khaolakkkkk@users.noreply.github.com>
2023-12-19 15:11:19 +0100

    Add files via upload

---
+++ Affichage.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Liste_Cartes = () => {
+  const [listeCartes, setListeCartes] = useState([]);
+  const [termeRecherche, setTermeRecherche] = useState('');
+  const [typeFiltre, setTypeFiltre] = useState('nom');
+  const [ordre_de_Tri, setOrdreTri] = useState('asc');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/api/cartes');
+        setListeCartes(response.data);
+      } catch (erreur) {
+        console.error('Erreur lors de la récupération de la liste de cartes Yu-Gi-Oh! :', erreur);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const cartes_Filtrees_Et_Trie = listeCartes
+    .filter((carte) => carte.nom.toLowerCase().includes(termeRecherche.toLowerCase()))
+    .sort((a, b) => {
+      switch (typeFiltre) {
+        case 'nom':
+          return ordre_de_Tri === 'asc' ? a.nom.localeCompare(b.nom) : b.nom.localeCompare(a.nom);
+        case 'prix':
+          const prixA = a.Prix ? parseFloat(a.Prix) : 0;
+          const prixB = b.Prix ? parseFloat(b.Prix) : 0;
+          return ordre_de_Tri === 'asc' ? prixA - prixB : prixB - prixA;
+        default:
+          return 0;
+      }
+    });
+
+  return (
+    <div>
+      <h1>Liste de Cartes Yu-Gi-Oh!</h1>
+
+      {/* Barre de recherche */}
+      <input
+        type="text"
+        placeholder="Rechercher par nom"
+        value={termeRecherche}
+        onChange={(e) => setTermeRecherche(e.target.value)}
+      />
+
+      {/* Options de tri */}
+      <div>
+        <label>
+          Trier par :
+          <select value={typeFiltre} onChange={(e) => setTypeFiltre(e.target.value)}>
+            <option value="nom">Nom</option>
+            <option value="prix">Prix</option>
+          </select>
+        </label>
+        <label>
+          Ordre :
+          <select value={ordre_de_Tri} onChange={(e) => setOrdreTri(e.target.value)}>
+            <option value="asc">Croissant</option>
+            <option value="desc">Décroissant</option>
+          </select>
+        </label>
+      </div>
+
+      <ul>
+        {cartes_Filtrees_Et_Trie.map((carte) => (
+          <li key={carte.id}>
+            <img src={carte.image_carte} alt={carte.nom} />
+            <div>
+              <p>Nom : {carte.nom}</p>
+              <p>Prix : {carte.Prix ? carte.Prix : 'N/A'}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Liste_Cartes;
